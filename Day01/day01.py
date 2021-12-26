62695db6bdd2098aa4e2791659e84079316c88ea
Taigen O <taigen@gmail.com>
2021-12-26 13:37:34 -0500

    Day01

---
+++ Day01/day01.py
@@ -0,0 +1,24 @@
+import itertools
+import sys
+
+with open('input.txt', 'r') as inputFile:
+    lines = [int(line.strip()) for line in inputFile]
+
+def part_one():
+    sum = 0
+    for i, line in enumerate(lines):
+        if i >= 1:
+            if lines[i] > lines[i-1]:
+                sum += 1
+    print(sum)
+
+def part_two():
+    sum = 0
+    for i, line in enumerate(lines):
+        if i >= 3:
+            if lines[i] + lines[i-1] + lines[i-2] > lines[i-1] + lines [i- 2] + lines[i- 3]:
+                sum += 1
+    print(sum)
+
+part_one()
+part_two()
